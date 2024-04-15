ae7d0aa46d6a1e5756584bbf5234d597117d5c47
Ignacio Cunial <igsecu@hotmail.com>
2024-04-15 16:59:28 -0300

    Created ViewAllJobs component

---
+++ src/components/ViewAllJobs.jsx
@@ -0,0 +1,9 @@
+const ViewAllJobs = () => {
+  return (
+    <section className="text-center">
+        <a href="/jobs" className="btn btn-dark text-white">View All Jobs</a>
+    </section>
+  )
+}
+
+export default ViewAllJobs
