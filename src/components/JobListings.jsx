ac3f42e34c3b36038beaf5a29f99d432ebdc1846
Ignacio Cunial <igsecu@hotmail.com>
2024-04-15 16:27:40 -0300

    Created JobsListings component

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,19 @@
+import jobs from "../jobs.json";
+import JobListing from "./JobListing";
+
+const JobListings = () => {
+  return (
+    <section className="p-5 text-center">
+        <h2 className="text-primary mb-4">Browse Jobs</h2>
+        <div className="container">
+            <div className="row g-4">
+                {jobs.map(job => (
+                    <JobListing key={job.id} job={job} />
+                ))}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default JobListings
