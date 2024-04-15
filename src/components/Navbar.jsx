fb56888e287326b50d93f1b43a2a7d49091de472
Ignacio Cunial <igsecu@hotmail.com>
2024-04-15 17:41:27 -0300

    Added text to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,12 @@ const Navbar = () => {
   return (
     <nav className="bg-primary navbar-dark py-3 border-bottom border-light">
         <div className="container d-flex flex-row justify-content-between align-items-center">
-            <a href="#">
-                <img src={logo} alt="React Logo" style={{ height: "30px"}}/>
-            </a>
+            <div className="d-flex align-items-center text-white">
+                <a href="#">
+                    <img src={logo} alt="React Logo" style={{ height: "30px"}}/>
+                </a>
+                <span className="fs-4 ms-2 d-none d-md-block">React Jobs</span>
+            </div>
             <ul className="nav nav-pills">
                 <li className="nav-item mx-1">
                     <a className="nav-link active" href="#">Home</a>
