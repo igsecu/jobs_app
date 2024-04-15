a129f378c1ab936fb07e7c8010f81d70273db026
Ignacio Cunial <igsecu@hotmail.com>
2024-04-15 15:26:30 -0300

    Created Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <section className="bg-primary text-light p-5 text-center">
+        <div className="container">
+            <h1>Become a React Dev</h1>
+            <p className="lead">Find the React job that fits your skills and needs</p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
