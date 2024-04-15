ad6df21b702d4ca21778c393e7fdabc4c3d19282
Ignacio Cunial <igsecu@hotmail.com>
2024-04-15 16:59:12 -0300

    Limited shown jobs to 3

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,12 +2,15 @@ import jobs from "../jobs.json";
 import JobListing from "./JobListing";
 
 const JobListings = () => {
+
+    const recentJobs = jobs.slice(0,3);
+
   return (
     <section className="p-5 text-center">
         <h2 className="text-primary mb-4">Browse Jobs</h2>
         <div className="container">
             <div className="row g-4">
-                {jobs.map(job => (
+                {recentJobs.map(job => (
                     <JobListing key={job.id} job={job} />
                 ))}
             </div>
