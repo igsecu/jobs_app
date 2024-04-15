4c6989cc7b068c0ee46dcc3784eac782530fc268
Ignacio Cunial <igsecu@hotmail.com>
2024-04-15 17:32:50 -0300

    Added bottom padding

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -1,6 +1,6 @@
 const ViewAllJobs = () => {
   return (
-    <section className="text-center">
+    <section className="text-center pb-5">
         <a href="/jobs" className="btn btn-dark text-white">View All Jobs</a>
     </section>
   )
