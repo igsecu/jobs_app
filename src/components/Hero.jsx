dbb590ca5c4255d289c4ca63c255bafdb769326e
Ignacio Cunial <igsecu@hotmail.com>
2024-04-15 15:57:45 -0300

    Updated Hero component -> added props

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
-const Hero = () => {
+const Hero = ({title, subtitle}) => {
   return (
     <section className="bg-primary text-light p-5 text-center">
         <div className="container">
-            <h1>Become a React Dev</h1>
-            <p className="lead">Find the React job that fits your skills and needs</p>
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
         </div>
     </section>
   )
