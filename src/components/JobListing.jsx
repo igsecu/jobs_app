35f7925d0f31f973baf949a1c878519d7ea9c247
Ignacio Cunial <igsecu@hotmail.com>
2024-04-15 17:11:08 -0300

    Updated JobListing -> added useState hook

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,10 +1,22 @@
+import {useState} from "react";
+
 const JobListing = ({job}) => {
+
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if(!showFullDescription) {
+    description = `${description.substring(0, 90)}...`;
+  }
+
   return (
     <div className="col-md-6 col-lg-4">
         <div className="card bg-light text-start p-2 border shadow">
             <small className="text-secondary">{job.type}</small>
             <h5 className="card-title fw-bold">{job.title}</h5>
-            <p className="fs-6">{job.description}</p>
+            <p className="fs-6 mb-0">{description}</p>
+            <p className="fw-bold text-end me-3" style={{cursor:"pointer"}}onClick={() => setShowFullDescription(!showFullDescription)}>{showFullDescription ? 'Less' : 'More'}</p>
             <p className="text-success">{job.salary} / Year</p>
             <p className="text-danger fw-bold">{job.location}</p>
             <a href={`/job/${job.id}`} className="btn btn-sm btn-primary text-white mb-3">Read More</a>
