646c9ae1441459d261955c6230346777aae161b3
Ignacio Cunial <igsecu@hotmail.com>
2024-04-16 19:10:06 -0300

    Created deleteJob function

--- src/App.js
+++ src/App.js
@@ -19,6 +19,14 @@ function App() {
 
     return;
   }
+
+  const deleteJob = async(id) => {
+    const res = await fetch(`http://localhost:8000/jobs/${id}`, {
+      method: "DELETE"
+    })
+
+    return;
+  }
   
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -26,7 +34,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />}/>
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />}/>
-        <Route path="/jobs/:id" element={<JobPage />}/>
+        <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob}/>}/>
         <Route path="*" element={<NotFoundPage />}/>
       </Route>
     )
