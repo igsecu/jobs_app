7bba0a891091083d751c93c6b216a82f1087ee01
Ignacio Cunial <igsecu@hotmail.com>
2024-04-16 13:56:05 -0300

    Added useState and useEffect to fetch jobs

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,7 +1,30 @@
-import jobs from "../jobs.json";
 import JobListing from "../components/JobListing";
 
+import {useState, useEffect} from "react";
+
 const JobsPage = () => {
+
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async() => {
+      try{
+
+        const res = await fetch("http://localhost:8000/jobs");
+        const data = await res.json();
+        setJobs(data);
+        
+      }catch(error){
+        console.log("Error fetching data", error);
+      }finally{
+        setLoading(false);
+      }
+    }
+
+    fetchJobs();
+  }, [])
+
   return (
     <section className="p-5 text-center">
       <h2 className="text-primary mb-4">Browse Jobs</h2>
