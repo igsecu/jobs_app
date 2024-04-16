3adcd91ae4f9ea1b4cece8a5a5811d689b46f37e
Ignacio Cunial <igsecu@hotmail.com>
2024-04-16 13:04:48 -0300

    Added to prop to Card

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -6,10 +6,10 @@ const HomeCards = () => {
         <div className="container">
             <div className="row g-4">
                 <div className="col-lg-6">
-                    <Card title="For Developers" subtitle="Browse our React Jobs and start your career today" button="Browse Jobs"/>
+                    <Card to="/jobs" title="For Developers" subtitle="Browse our React Jobs and start your career today" button="Browse Jobs"/>
                 </div>
                 <div className="col-lg-6">
-                    <Card title="For Employers" subtitle="List your job to find the perfect developer for the role" button="Add Job"/>
+                    <Card to="/add-job" title="For Employers" subtitle="List your job to find the perfect developer for the role" button="Add Job"/>
                 </div>
             </div>
         </div>
