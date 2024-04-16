aac8cb375ca2dd9c5a1615de1e86b64298229bdc
Ignacio Cunial <igsecu@hotmail.com>
2024-04-16 13:06:49 -0300

    Added Link component

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -1,7 +1,9 @@
+import {Link} from "react-router-dom";
+
 const ViewAllJobs = () => {
   return (
     <section className="text-center pb-5">
-        <a href="/jobs" className="btn btn-dark text-white">View All Jobs</a>
+        <Link to="/jobs" className="btn btn-dark text-white">View All Jobs</Link>
     </section>
   )
 }
