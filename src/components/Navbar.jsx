906a042cc953053fad99653e012d53e16db33ea1
Ignacio Cunial <igsecu@hotmail.com>
2024-04-16 13:20:21 -0300

    Added NavLink -> active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,25 @@
 import logo from "../assets/images/react.png"
-import {Link} from "react-router-dom"
+import {NavLink} from "react-router-dom"
 
 const Navbar = () => {
   return (
     <nav className="bg-primary navbar-dark py-3 border-bottom border-light">
         <div className="container d-flex flex-row justify-content-between align-items-center">
             <div className="d-flex align-items-center text-white">
-                <Link to="/">
+                <NavLink to="/">
                     <img src={logo} alt="React Logo" style={{ height: "30px"}}/>
-                </Link>
+                </NavLink>
                 <span className="fs-4 ms-2 d-none d-md-block">React Jobs</span>
             </div>
             <ul className="nav nav-pills">
                 <li className="nav-item mx-1">
-                    <Link className="nav-link active" to="/">Home</Link>
+                    <NavLink className="nav-link" to="/">Home</NavLink>
                 </li>
                 <li className="nav-item  mx-1">
-                    <Link className="nav-link" to="/jobs">Jobs</Link>
+                    <NavLink className="nav-link" to="/jobs">Jobs</NavLink>
                 </li>
                 <li className="nav-item  mx-1">
-                    <Link className="nav-link" to="/add-job">Add Job</Link>
+                    <NavLink className="nav-link" to="/add-job">Add Job</NavLink>
                 </li>
             </ul>
         </div>
