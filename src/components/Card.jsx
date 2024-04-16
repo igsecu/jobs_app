13fece5ee1702ead013da15b6ca9e144257df044
Ignacio Cunial <igsecu@hotmail.com>
2024-04-16 13:04:38 -0300

    Added Link component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,12 @@
-const Card = ({title, subtitle, button}) => {
+import {Link} from "react-router-dom"
+
+const Card = ({title, subtitle, button, to}) => {
   return (
     <div className="card bg-transparent" style={{boxShadow: "1px 2px 5px gray"}}>
         <div className="card-body">
             <h3 className="card-title">{title}</h3>
             <p className="card-text fw-bold">{subtitle}</p>
-            <button className="btn btn-dark text-white">{button}</button>
+            <Link to={to} className="btn btn-dark text-white">{button}</Link>
         </div>
     </div>
   )
