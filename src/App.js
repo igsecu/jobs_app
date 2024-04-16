c41a4a84a86062598285026b37a5dc5f436b31b9
Ignacio Cunial <igsecu@hotmail.com>
2024-04-16 12:48:32 -0300

    Added router configuration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import Navbar from "./components/Navbar.jsx";
-import Hero from "./components/Hero.jsx";
-import HomeCards from "./components/HomeCards.jsx";
-import JobListings from "./components/JobListings.jsx";
-import ViewAllJobs from "./components/ViewAllJobs.jsx";
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
+
+import HomePage  from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
-   <>
-    <Navbar />
-    <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs" />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
-   </>
+    <RouterProvider router={router}/>
+ 
   );
 }
 
