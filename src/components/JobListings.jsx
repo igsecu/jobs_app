422dffece4f5d40dc258a698677dfbda72c024b5
Ignacio Cunial <igsecu@hotmail.com>
2024-04-16 13:55:51 -0300

    Added useEffect and useState to fetch jobs

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,16 +1,36 @@
-import jobs from "../jobs.json";
 import JobListing from "./JobListing";
+
+import {useState, useEffect} from "react";
 
 const JobListings = () => {
 
-    const recentJobs = jobs.slice(0,3);
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJobs = async() => {
+        try{
+
+            const res = await fetch("http://localhost:8000/jobs");
+            const data = await res.json();
+            setJobs(data.slice(0,3));
+            
+        }catch(error){
+            console.log("Error fetching data", error);
+        }finally{
+            setLoading(false);
+        }
+        }
+
+        fetchJobs();
+    }, [])
 
   return (
     <section className="p-5 text-center">
         <h2 className="text-primary mb-4">Browse Jobs</h2>
         <div className="container">
             <div className="row g-4">
-                {recentJobs.map(job => (
+                {jobs.map(job => (
                     <JobListing key={job.id} job={job} />
                 ))}
             </div>
